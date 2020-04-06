c6dfab6ccdfb6c95a4cf6cd1eb21b644d26854af
Anastasiia Loginova <barbisize@gmail.com>
2020-04-06 11:18:58 +0300

    Исправлена валидация в роутах

--- routes/cards.js
+++ routes/cards.js
@@ -8,7 +8,7 @@ router.get('/', auth, getCards);
 router.post('/', auth, celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().url(),
+    link: Joi.string().required().uri(),
   }),
 }), createCard);
 router.delete('/:id', auth, celebrate({
--- routes/registration.js
+++ routes/registration.js
@@ -6,7 +6,7 @@ router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().required().url(),
+    avatar: Joi.string().required().uri(),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
